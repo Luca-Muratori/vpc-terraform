220beb0acbd2636c7a29c6c447388218f7e286c0
Luca Muratori <luca.muratori.94@hotmail.it>
2024-10-13 12:10:47 +0200

    retry vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   region = "eu-central-1"
-  
+
 }
 
 //Point 2 'VPC Configuration'-----
 module "vpc" {
   source = "./modules/vpc"
+  
 }
 
 //-----------------------------------
