de2cd1702f31a35e515a46fa17b370d614357005
Luca Muratori <luca.muratori.94@hotmail.it>
2024-10-13 12:28:33 +0200

    create-vpc-2

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,4 @@
 output "vpc_id" {
   description = "ID of the project VPC-1"
   value = module.vpc
-  
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -14,36 +14,35 @@ variable "type" {
 }
 
 variable "subnet"{
-    type = list(object({
-        name=string,
-        cidr=string
-    }))
-    default = [ 
-      {
-        name = "Public1A",
-        cidr = "10.0.0.1/24"
-      },
-      {
-        name = "Private2A",
-        cidr = "10.0.0.2/24"
-      },
-      {
-        name = "Private3A",
-        cidr = "10.0.0.3/24"
-      },
-      {
-        name = "Public1B",
-        cidr = "10.0.0.4/24"
-      },
-      {
-        name = "Private2B",
-        cidr = "10.0.0.5/24"
-      },
-      {
-        name = "Private3B",
-        cidr = "10.0.0.6/24"
-      },
-    ]
-
+  type = list(object({
+      name=string,
+      cidr=string
+  }))
+  default = [ 
+    {
+      name = "Public1A",
+      cidr = "10.0.0.1/24"
+    },
+    {
+      name = "Private2A",
+      cidr = "10.0.0.2/24"
+    },
+    {
+      name = "Private3A",
+      cidr = "10.0.0.3/24"
+    },
+    {
+      name = "Public1B",
+      cidr = "10.0.0.4/24"
+    },
+    {
+      name = "Private2B",
+      cidr = "10.0.0.5/24"
+    },
+    {
+      name = "Private3B",
+      cidr = "10.0.0.6/24"
+    },
+  ]
 }
 //end part 1---------------------------
