cbde11c0a7058eda493584c056c4356047dbe02e
Luca Muratori <luca.muratori.94@hotmail.it>
2024-10-14 21:39:44 +0200

    created the first logic for the creation of the subnet-3

--- terraform/modules/subnet/variables.tf
+++ terraform/modules/subnet/variables.tf
@@ -6,15 +6,15 @@ variable "subnet_cidr" {
   default = [
     {
       name = "Public1A",
-      cidr = "10.0.0.0/24"
+      cidr = "10.0.1.0/24"
     },
     {
       name = "Private2A",
-      cidr = "10.0.0.0/24"
+      cidr = "10.0.2.0/24"
     },
     {
       name = "Private3A",
-      cidr = "10.0.0.0/24"
+      cidr = "10.0.3.0/24"
     },
   ]
 }
