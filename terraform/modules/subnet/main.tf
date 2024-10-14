3df36695f7818e21497efbcd9a77a16c05d1a050
Luca Muratori <luca.muratori.94@hotmail.it>
2024-10-14 21:33:29 +0200

    created the first logic for the creation of the subnet

--- terraform/modules/subnet/main.tf
+++ terraform/modules/subnet/main.tf
@@ -8,7 +8,7 @@ data "aws_vpc" "tf-vpc-project" {
 
 resource "aws_subnet" "subnet1" {
   vpc_id                  = data.aws_vpc.tf-vpc-project.id
-  cidr_block              = var.subnet_cidr.default["Public1A"]
+  cidr_block              = var.subnet_cidr["Public1A"].cidr_block
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
   tags = {
@@ -18,7 +18,7 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet2" {
   vpc_id                  = data.aws_vpc.tf-vpc-project.id
-  cidr_block              = var.subnet_cidr.default["Public1B"]
+  cidr_block              = var.subnet_cidr["Public1B"].cidr_block
   availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = false
   tags = {
@@ -28,7 +28,7 @@ resource "aws_subnet" "subnet2" {
 
 resource "aws_subnet" "subnet3" {
   vpc_id                  = data.aws_vpc.tf-vpc-project.id
-  cidr_block              = var.subnet_cidr.default["Public1C"]
+  cidr_block              = var.subnet_cidr["Public1C"].cidr_block
   availability_zone       = "eu-central-1c"
   map_public_ip_on_launch = true
   tags = {
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,1 @@
-output "vpc_id" {
-  description = "ID of the project VPC-1"
-  value       = module.vpc.vpc_id
-}
+
