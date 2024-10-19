45c930e661e429ec8fcd6f890db6f566cd9e4d5b
Luca Muratori <luca.muratori.94@hotmail.it>
2024-10-19 11:23:08 +0200

    create ec2-6

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -39,36 +39,36 @@ data "aws_security_group" "private_3a" {
     values = ["private_3a-sg-ssh"]
   }
 }
-////////////
+//////////
 resource "aws_instance" "public1a" {
-  ami = var.ami
-  instance_type = "t2.micro"
-  subnet_id = data.aws_subnet.public1a.id
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.public1a.id
   vpc_security_group_ids = [data.aws_security_group.public_1a.id]
-  key_name = "terraform"
-  tags ={
+  key_name               = "terraform"
+  tags = {
     Name = "web_public1a"
   }
 }
 
 resource "aws_instance" "private2a" {
-  ami = var.ami
-  instance_type = "t2.micro"
-  subnet_id = data.aws_subnet.private2a.id
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.private2a.id
   vpc_security_group_ids = [data.aws_security_group.private_2a.id]
-  key_name = "terraform"
-  tags ={
+  key_name               = "terraform"
+  tags = {
     Name = "app_private2a"
   }
 }
 
 resource "aws_instance" "private3a" {
-  ami = var.ami
-  instance_type = "t2.micro"
-  subnet_id = data.aws_subnet.private3a.id
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.private3a.id
   vpc_security_group_ids = [data.aws_security_group.private_3a.id]
-  key_name = "terraform"  
-  tags ={
+  key_name               = "terraform"
+  tags = {
     Name = "data_private3a"
   }
 }
--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,5 +1,5 @@
 variable "ami" {
   description = "ami id for the ec2 instances"
-  type = string
-  default = "ami-08ec94f928cf25a9d"
+  type        = string
+  default     = "ami-08ec94f928cf25a9d"
 }
